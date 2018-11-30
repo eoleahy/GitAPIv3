58f53760c997b8cb6fea297e62d722f9d6a83e15
eoleahy1 <eoleahy@tcd.ie>
2018-11-30 17:37:45 +0000

    added additional output

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,6 @@
 from github import Github
 import getpass
+import json
 
 # using username and password
 def main():
@@ -7,10 +8,16 @@ def main():
     pswd = getpass.getpass('Password:')
     g = Github(username, pswd)
 
-    for repo in g.get_user().get_repos():
+    totalRepoList = None
+    user = g.get_user()
+    repos = user.get_repos()
+    for repo in repos:
         print(repo.name)
         #repo.edit(has_wiki=False)
         # to see all the available attributes and methods
+        contrib = repo.get_contributors()
+        for users in contrib:
+            print("     ", users.name)
 
     if(input('Press q to close: ') is 'q'):
         exit()
