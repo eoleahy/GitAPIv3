680f55525b0c61ceb7c14799f81e8f2554451b8f
eoleahy1 <eoleahy@tcd.ie>
2018-12-02 23:00:15 +0000

    completed program

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # GitInterrogator
 
-Takes a git login and prints info about account using Github API v3
+Takes a git login from Flask Http server.
+Information about repositories is put into Json
+file and is then passed to d3 javascript and visualised
+
+
+-----Some necessary installs-------
+
+pip install flask
+
+pip install pygithub
+
+
 ./run.sh to run
--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,5 @@
 from github import Github
 from flask import Flask, render_template,request
-import getpass  #no show password
 import json     #json for data
 import webbrowser #open web page
 import os       #get cwd
@@ -10,16 +9,26 @@ app = Flask(__name__)
 # using username and password
 @app.route('/',methods=['POST','GET'])
 def home():
-    errors=[]
-    results={}
+
 
     if request.method == "POST":
         username = request.form['username']
         pswd = request.form['password']
 
         handle_input(username,pswd)
+        json_data = send_Json()
+        return render_template('socialGraph.html', json_data= json_data)
 
-    return render_template('socialGraph.html',errors=errors,results=results)
+    return render_template('socialGraph.html')
+
+def send_Json():
+
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT,"static/","data.json")
+    data =json.load(open(json_url))
+    print("sending json")
+
+    return data
 
 
 def handle_input(username,pswd):
@@ -27,67 +36,30 @@ def handle_input(username,pswd):
     g = Github(username,pswd)
     user = g.get_user()
 
-    total_user_list = []
-    total_repo_list = []
     json_arr=[]
     my_repositories = user.get_repos()
-
-    total_repo_list.append(my_repositories)
 
     #print logged in user as the "root"
     print(user.login)
     #iterate through repos
     for my_repo in my_repositories:
 
-        #avoid overlap by comparing my repos to the total list
-        if(my_repo not in total_repo_list):
-            total_repo_list.append(my_repo)
-            print(" |_", my_repo.name)
-            json_repo = {
-                "repo": my_repo.name,
-                "link_repo": "root",
-                "owner": username
-            }
-            y = json.dumps(json_repo)
-            json_arr.append(y)
-        #    print(y)
-
-        #contributors of each repo iterate similarly
+        i = 0
         contributors = my_repo.get_contributors()
         for users_contrib in contributors:
+            i+=1
 
-            #avoiding recursion
-            if(users_contrib not in total_user_list and user.login != users_contrib.login):
-                total_user_list.append(users_contrib)
-                print("    |_", users_contrib.login)
+        json_repo = {
+            "name": my_repo.name,
+            "value": i
+        }
 
-                #getting contributors' repos
-                contrib_repos = users_contrib.get_repos()
+        json_arr.append(json_repo)
 
-                for repo in contrib_repos:
-                    if(repo not in total_repo_list):
-                        total_repo_list.append(repo)
-                        print("     |_", repo.name)
-
-                        json_repo = {
-                            "repo": repo.name,
-                            "link_repo": my_repo.name,
-                            "owner": users_contrib.login
-                        }
-                        y = json.dumps(json_repo)
-                        json_arr.append(y)
-
-    print(total_user_list)
-    '''
-    for i in total_repo_list:
-        print(i)
-
-    '''
-    for j in json_arr:
-        print(j)
-
-    with open('src/data.json','w') as outfile:
+    with open('src/static/data.json','w') as outfile:
         json.dump(json_arr, outfile)
+
+
 
 
 if __name__ == '__main__':
